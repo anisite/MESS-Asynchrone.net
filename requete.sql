aa5c87540d3c272e6790f4cc9dcd26553354f03f
anisite <infologique@gmail.com>
2017-10-19 08:01:29 -0400

    Update requete.sql

--- requete.sql
+++ requete.sql
@@ -2,12 +2,12 @@ WITH CONVOI
      AS (SELECT [CA_NM_LIBL_SERV_ASYN], [CA_D_HR_EXEC_PLAN], [CA_E_REQT], [CA_C_TYPE_REQT_ASYN],
                 Row_number()
                   OVER( --OVER permet d'ajouter des numéros de lignes sur la partition (équivalent d'un group by)
-                    PARTITION BY CASE WHEN[CA_C_MODE_REQT_CONT] = 'AvecConvoi'
+                    PARTITION BY CASE WHEN [R].[CA_C_MODE_REQT_CONT] = 'AvecConvoi'
                                     THEN [R].[CA_N_IDEN_CONV] -- Numéro applicatif du convoi
-                                    ELSE convert(varchar(3),ABS(CHECKSUM(NEWID())) % {pourcentageMax}) -- Dans le cas d'un traitement SansConvoi, crée un convoi éphémère
+                                    ELSE [R].[CA_N_EVEN_AFFR_RECP] + CONVERT(varchar(3),ABS(CHECKSUM(NEWID())) % {largeurParEvenementAffaire}) -- Dans le cas d'un traitement SansConvoi, crée un convoi éphémère
                                  END
                     ORDER BY[CA_D_INSC_REQT] ) AS NumeroLigne
-         FROM   [dbo].[CA1_REQUETE_ASYNCHRONE] R
+         FROM [dbo].[CA1_REQUETE_ASYNCHRONE] R
          WHERE [CA_D_HR_FIN_REQT] IS NULL --Sélectionner seulement les éléments actifs
          ),
      A_TRAITER
