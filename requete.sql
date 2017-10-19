9fcfceab59343575ad715cf57f7757156d4b226d
anisite <infologique@gmail.com>
2017-10-19 08:15:08 -0400

    Update requete.sql

--- requete.sql
+++ requete.sql
@@ -4,7 +4,7 @@ WITH CONVOI
                   OVER( --OVER permet d'ajouter des numéros de lignes sur la partition (équivalent d'un group by)
                     PARTITION BY CASE WHEN [R].[CA_C_MODE_REQT_CONT] = 'AvecConvoi'
                                     THEN [R].[CA_N_IDEN_CONV] -- Numéro applicatif du convoi
-                                    ELSE [R].[CA_N_EVEN_AFFR_RECP] + CONVERT(varchar(3),ABS(CHECKSUM(NEWID())) % {largeurParEvenementAffaire}) -- Dans le cas d'un traitement SansConvoi, crée un convoi éphémère
+                                    ELSE [R].[CA_N_EVEN_AFFR_RECP] + CONVERT(varchar(3),ABS(CHECKSUM(NEWID())) % {maximumParEvenementAffaire}) -- Dans le cas d'un traitement SansConvoi, crée un convoi éphémère
                                  END
                     ORDER BY[CA_D_INSC_REQT] ) AS NumeroLigne
          FROM [dbo].[CA1_REQUETE_ASYNCHRONE] R
